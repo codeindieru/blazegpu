0f254cffb12f868272d63a0e21fa3fa74c88f021
codeindieru <codeindieru@gmail.com>
2025-04-30 00:24:29 +0300

    Basic framework methods for working with shaders have been created

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,78 @@
 export class BlazeGPU {
-    constructor() {
-        console.log('BlazeGPU initialized');
+    canvas: HTMLCanvasElement;
+    context: GPUCanvasContext;
+    adapter: GPUAdapter;
+    device: GPUDevice;
+    presentationFormat: string;
+    module: GPUShaderModule;
+    pipeline: GPURenderPipeline;
+
+    constructor(canvas: HTMLCanvasElement) {
+        this.canvas = canvas;
+    }
+    
+    async init() {
+        this.adapter = await navigator.gpu?.requestAdapter();
+        this.device = await this.adapter?.requestDevice();
+
+        if (!this.device) {
+            console.log("Browser does not support WebGPU");
+        }
+
+        this.context = this.canvas.getContext("webgpu");
+
+        this.presentationFormat = navigator.gpu.getPreferredCanvasFormat();
+
+        this.context.configure({
+            device: this.device,
+            format: this.presentationFormat,
+        });
+    }
+
+    createShader(name: string, wgsl: string) {
+        this.module = this.device.createShaderModule({
+            label: name,
+            code: wgsl
+        });
+    }
+
+    createPipeline() {
+        this.pipeline = this.device.createRenderPipeline({
+            label:"Simple pipeline",
+            layout: "auto",
+            vertex: {
+                module: this.module,
+                entryPoint: "vs",
+            },
+            fragment: {
+                module: this.module,
+                entryPoint: "fs",
+                targets: [{ format: this.presentationFormat }],
+            }
+        })
+    }
+
+    render() {
+        const renderPassDescriptor = {
+            label: "Simple render pass",
+            colorAttachments: [{
+                clearValue: [0.3, 0.3, 0.3, 1.0],
+                loadOp: "clear",
+                storeOp: "store",
+            }]
+        }
+
+        renderPassDescriptor.colorAttachments[0].view = this.context.getCurrentTexture().createView();
+
+        const encoder = this.device.createCommandEncoder({ label: "Simple encoder" });
+
+        const pass = encoder.beginRenderPass(renderPassDescriptor);
+        pass.setPipeline(this.pipeline);
+        pass.draw(3);
+        pass.end();
+
+        const commandBuffer = encoder.finish();
+
+        this.device.queue.submit([commandBuffer]);
     }
 }
